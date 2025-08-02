23ff9d5e3bde5d88d4dd4ab296f85effd9d18bb0
osirisofficial <kondamanas.official@gmail.com>
2025-08-02 17:26:33 +0530

    created terrafrom block and setuped backend block too

---
+++ project-1/infrastructure/vpc/c1-versions.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "~> v1.12"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  #backend block
+  backend "s3" {
+    name = "devops-aws-manas"
+    key = "project-1/terraform.tfstate"
+    region = "us-east1"
+  }
+
+}
+
+provider "aws" {
+    profile = "default"
+  region = "us-east-1"
+}
