534a83c83611730979362486c24aa62b3568f066
osirisofficial <kondamanas.official@gmail.com>
2025-08-03 00:02:17 +0530

    created ec2 instance for jenkins and setupped using script

---
+++ project-1/infrastructure/jenkins-ec2/c5-03-jenkins-ec2-module.tf
@@ -0,0 +1,34 @@
+# AWS EC2 Instance Terraform Module
+# Bastion Host - EC2 Instance that will be created in VPC Public Subnet
+module "public_bastion_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "6.0.2"
+  # insert the 10 required variables here
+  name = "jenkins_server"
+
+  
+  ami = "ami-0a7d80731ae1b2435"
+  
+  #instance type 
+  instance_type = "t2.micro"
+
+  #key pair
+  key_name = "project-1-key"
+
+  #public subnet id to create instance their
+  subnet_id = data.terraform_remote_state.vpc.outputs.public_subnets[0]
+
+  #public SG-id attached to public subnet in vp
+  vpc_security_group_ids = [module.jenkins-sg.security_group_id]
+
+  #userdata
+  user_data = file("${path.module}/install-build-tool.sh")
+
+  #tags
+  tags = {
+    name = "jenkins_server"
+    Terraform = "true"
+    envrionment = "Dev"
+  }
+
+}
---
+++ project-1/infrastructure/jenkins-ec2/install-build-tool.sh
@@ -0,0 +1,69 @@
+# jenkins installation
+sudo apt update -y
+sudo apt install openjdk-17-jre -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# git
+sudo apt install
+git --version
+
+# docker 
+sudo apt update -y
+sudo apt install docker.io -y
+sudo systemctl enable docker
+sudo systemctl start docker
+
+sudo usermod -aG docker ubuntu # to add this user to docker group  so it can execute docker command
+newgrp docker 
+
+# sonar
+docker run --name sonar -p 9000:9000 sonarqube:lts-community
+
+# aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+#terraform cli
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform
+
+# Installing kubectl
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
+sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+
+# Installing Trivy
+# Ref - https://aquasecurity.github.io/trivy-repo/
+sudo tee /etc/yum.repos.d/trivy.repo << 'EOF'
+[trivy]
+name=Trivy repository
+baseurl=https://aquasecurity.github.io/trivy-repo/rpm/releases/$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://aquasecurity.github.io/trivy-repo/rpm/public.key
+EOF
+
+sudo yum -y update
+sudo yum -y install trivy
+
+# Intalling Helm
+# Ref - https://helm.sh/docs/intro/install/
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
