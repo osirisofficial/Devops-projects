25d2ee2f5aaca85d6e3dd69825112b5f129d8e9a
osirisofficial <kondamanas.official@gmail.com>
2025-08-02 20:29:19 +0530

    created terrafrom block and setuped backend, also implemented datasource to get vpc details from its remote state

---
+++ project-1/infrastructure/jenkins-ec2/c1-versions.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = "~> v1.12"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  #backend block
+  backend "s3" {
+    bucket = "devops-aws-manas"
+    key = "project-1/jenkins-ec2/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+    profile = "default"
+  region = "us-east-1"
+}
+
+# datasource  : to get vpc deatils from remote state
+data "terraform_remote_state" "vpc" {
+    backend = "s3"
+    config = {
+      bucket = "devops-aws-manas"
+      key = "project-1/vpc/terraform.tfstate"
+      region = "us-east-1"
+    }
+}
