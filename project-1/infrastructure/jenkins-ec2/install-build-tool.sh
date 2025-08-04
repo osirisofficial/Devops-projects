f22739246c1412e8d6bd9ac04927aaf3c527eeb1
osirisofficial <kondamanas.official@gmail.com>
2025-08-04 17:41:15 +0530

    fixed error in sonar container inialization, changed installation of kubectl, trivy was installed via yum now changed to apt version

--- project-1/infrastructure/jenkins-ec2/install-build-tool.sh
+++ project-1/infrastructure/jenkins-ec2/install-build-tool.sh
@@ -30,6 +30,7 @@ newgrp docker
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 
 # aws cli
+sudo apt install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
