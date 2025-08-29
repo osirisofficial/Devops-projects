2a23a74374753f81c91de6366dc3be1febb39ae8
osirisofficial <kondamanas.official@gmail.com>
2025-08-29 17:52:06 +0530

    setupped terraform block and backend block

---
+++ project-2/infrastructure/ec2/anisble/c1-version.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~> v1.12"
+
+  required_providers {
+     aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "devops-aws-manas"
+    key = "project-1/ec2/ansible/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
