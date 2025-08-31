26981c28661e8acb904429bbcfa45c06cb79cdfa
osirisofficial <kondamanas.official@gmail.com>
2025-08-31 23:59:19 +0530

    changed instance type

--- project-2/infrastructure/ec2/nexus/c5-03-ec2-module.tf
+++ project-2/infrastructure/ec2/nexus/c5-03-ec2-module.tf
@@ -9,7 +9,7 @@ module "sonar-nexus-ec2" {
   ami = "ami-0a7d80731ae1b2435"
   
   #instance type 
-  instance_type = "t2.medium"
+  instance_type = "t2.large"
 
   #key pair
   key_name = "project-2-key"
