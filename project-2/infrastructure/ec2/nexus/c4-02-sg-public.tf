ce40c2ac4d4945e2c75ad611f181fd5dedbc51cf
osirisofficial <kondamanas.official@gmail.com>
2025-08-31 14:29:14 +0530

    made changes in name and ports

--- project-2/infrastructure/ec2/nexus/c4-02-sg-public.tf
+++ project-2/infrastructure/ec2/nexus/c4-02-sg-public.tf
@@ -1,8 +1,8 @@
-module "nexus_sg" {
+module "sonar_nexus_sg" {
   source = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
 
-  name        = "user-service"
+  name        = "sg-for-sonar-nexus"
   description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
 
@@ -17,6 +17,13 @@ module "nexus_sg" {
       description = "nexus"
       cidr_blocks = "0.0.0.0/0"
     },
+    {
+      from_port   = 9000
+      to_port     = 9000
+      protocol    = "tcp"
+      description = "sonarqube"
+      cidr_blocks = "0.0.0.0/0"
+    }
   ]
   
   egress_rules = ["all-all"]
