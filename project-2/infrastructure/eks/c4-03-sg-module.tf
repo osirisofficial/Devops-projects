096ca0a8a43547185c9e50549887d1df2dd8f397
osirisofficial <kondamanas.official@gmail.com>
2025-09-02 15:01:42 +0530

    created sg for eks

---
+++ project-2/infrastructure/eks/c4-03-sg-module.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "eks_public_access_sg" {
+  name        = "eks-api-access"
+  description = "Allow access to EKS API server"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    description = "Allow EKS API access from my IP"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.vpc_cidr_block] # replace with your IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
