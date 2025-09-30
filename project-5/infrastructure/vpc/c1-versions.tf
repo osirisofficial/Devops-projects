8d56e60692764fa4138073055544af13f4aa16b6
osirisofficial <kondamanas.official@gmail.com>
2025-09-30 20:51:58 +0530

    created vpc

---
+++ project-5/infrastructure/vpc/c1-versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = "~>v1.12"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "manas-devops-projects"
+     key = "project-5/vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
