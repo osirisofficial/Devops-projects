c7b5009485b22bf22170376605e2429c84b812f3
osirisofficial <kondamanas.official@gmail.com>
2025-10-03 19:13:22 +0530

    minior error fixed

--- project-5/infrastructure/ec2/ansible/c6-03-ec2-module.tf
+++ project-5/infrastructure/ec2/ansible/c6-03-ec2-module.tf
@@ -5,7 +5,7 @@ module "ansible-ec2" {
   name = "ansible-ec2"
 
   ami                    = "ami-0bbdd8c17ed981ef9"
-  instance_type          = "t2.mini" 
+  instance_type          = "t2.micro" 
   subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnets[0]
   vpc_security_group_ids = [module.ansible_sg.security_group_id]
   associate_public_ip_address = true
